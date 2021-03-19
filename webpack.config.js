aaaf800acb33d9cb3ceb8c79613103bd5752b6b6
Pavel Khrebto <pavlo.khrebto@ujet.lu>
2021-03-19 17:55:22 +0200

    Add dotenv module + webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const webpack = require('webpack');
+const dotenv = require('dotenv');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -26,6 +28,12 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.join(__dirname, 'public', 'index.html')
+        }),
+        new webpack.DefinePlugin({
+            'process.env': JSON.stringify(dotenv.config().parsed) // it will automatically pick up key values from .env file
         })
-    ]
+    ],
+    devServer: {
+        contentBase: './dist',
+    },
 }
