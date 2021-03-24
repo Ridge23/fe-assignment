e148e89d4c547e319994bbaf6b96f4afe10ec055
Pavel Khrebto <pavlo.khrebto@ujet.lu>
2021-03-24 22:50:29 +0200

    Add tests for Filter component

--- src/components/filter/Filter.test.tsx
+++ src/components/filter/Filter.test.tsx
@@ -6,9 +6,11 @@ import Filter from './Filter';
 import InputText from '../form/InputText'
 import Button from '../form/Button';
 
+const handleChange = jest.fn();
+
 describe('Filter', () => {
     it('should render correctly', () => {
-        const component = mount(<Filter handleChange={()=>{}} />);
+        const component = mount(<Filter handleChange={handleChange} />);
         
         expect(component.childAt(0).hasClass('filter')).toBe(true);
         expect(component.contains(<input type="text" />));
@@ -16,5 +18,13 @@ describe('Filter', () => {
         expect(component.contains(<Button caption='Reset' onClick={() => {}} />));
 
         expect(component).toMatchSnapshot();
+
+        const input = component.find('input.input-text');
+        const button = component.find('button.button');
+        input.simulate('change', { target: { value: 'He' } });
+        input.simulate('change', { target: { value: 'llo' } });
+        button.simulate('click');
+        
+        expect(handleChange).toHaveBeenCalledTimes(3);
     });
 });
