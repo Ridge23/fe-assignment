e7d4b94fd0fd1c0583578e5700a1f9be943bee15
Pavel Khrebto <pavlo.khrebto@ujet.lu>
2021-03-24 16:45:45 +0200

    Add full-render test for filter

--- src/components/filter/Filter.test.tsx
+++ src/components/filter/Filter.test.tsx
@@ -1,5 +1,6 @@
+import 'jsdom-global/register';
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 
 import Filter from './Filter';
 import InputText from '../form/InputText'
@@ -7,9 +8,9 @@ import Button from '../form/Button';
 
 describe('Filter', () => {
     it('should render correctly', () => {
-        const component = shallow(<Filter handleChange={()=>{}} />);
+        const component = mount(<Filter handleChange={()=>{}} />);
         
-        expect(component.hasClass('filter')).toBe(true);
+        expect(component.childAt(0).hasClass('filter')).toBe(true);
         expect(component.contains(<input type="text" />));
         expect(component.contains(<InputText value='' onChange={() => {}} />));
         expect(component.contains(<Button caption='Reset' onClick={() => {}} />));
